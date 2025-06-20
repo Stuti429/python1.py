63c23e11df9e46151904c0b26a6eb4fe82a3edb2
Stuti429 <jaiswalstuti74@gmail.com>
2025-06-20 23:23:32 +0530

    Add files via upload

---
+++ data2.py
@@ -0,0 +1,7 @@
+numbers = list (range(1,11))
+first_five = numbers[:5]
+reversed_five = numbers[::-1]
+print("Original list:",first_five)
+print("Extracted list:",reversed_five)
+print("Reversed list",reversed_five)
+
